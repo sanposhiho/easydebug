171bb46d4626b411d978aabfb280074799165d85
sanposhiho <handbomusic@gmail.com>
2020-09-05 15:30:56 +0900

    fix: fix sampledata

--- sample.go
+++ sample.go
@@ -2,17 +2,13 @@ package main
 
 func test() int {
 	hoge := 1
-	dmp("hoge", hoge)
 
 	fuga := 3
-	dmp("fuga", fuga)
 
 	if hoge == 2 {
 		fuga = abusoluteTwo()
-		dmp("fuga", fuga)
 
 		hoge = 12
-		dmp("hoge", hoge)
 
 	}
 
@@ -21,12 +17,4 @@ func test() int {
 
 func abusoluteTwo() int {
 	return 2
-}
-
-// generated from goeasydebug
-// function for data dump
-func dmp(valueName string, v ...interface{}) {
-  for _, vv := range(v) {
-      fmt.Printf("%s: %#v\n",valueName, vv)
-  }
 }
